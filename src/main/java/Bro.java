9445570169d733aa4d23ade7915391435fdacc9e
denzelgoo <denzelgoo05@gmail.com>
2026-08-21 03:12:28 +0800

    Update the chatbot to greet the user and exit

--- src/main/java/Bro.java
+++ src/main/java/Bro.java
@@ -1,10 +1,20 @@
 public class Bro {
     public static void main(String[] args) {
+        String line = "____________________________________________________________";
+
         String banner = "    ____   ____  ____ \n"
                 + "   / __ ) / __ \\/ __ \\\n"
                 + "  / __  |/ /_/ / / / /\n"
                 + " / /_/ // _, _/ /_/ / \n"
                 + "/_____//_/ |_|\\____/  \n";
+        System.out.println(line);
         System.out.println(banner);
+
+        // Greet the user and exit
+        System.out.println("What's up bro, I'm Bro.");
+        System.out.println("If you need anything, just ask bro.");
+        System.out.println(line);
+        System.out.println("See you soon bro.");
+        System.out.println(line);
     }
 }
