cdd0006d859a864cb69fe9e04d7981663a2ede4b
denzelgoo <denzelgoo05@gmail.com>
2026-08-21 03:04:45 +0800

    Rename chatbot from Duke to Bro

---
+++ src/main/java/Bro.java
@@ -0,0 +1,10 @@
+public class Bro {
+    public static void main(String[] args) {
+        String banner = "    ____   ____  ____ \n"
+                + "   / __ ) / __ \\/ __ \\\n"
+                + "  / __  |/ /_/ / / / /\n"
+                + " / /_/ // _, _/ /_/ / \n"
+                + "/_____//_/ |_|\\____/  \n";
+        System.out.println(banner);
+    }
+}
