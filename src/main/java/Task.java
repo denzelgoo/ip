30221e5aa8e907c839ae62379e8fe6508b56c1bb
denzelgoo <denzelgoo05@gmail.com>
2026-08-21 10:04:57 +0800

    Add Task class to represent tasks

---
+++ src/main/java/Task.java
@@ -0,0 +1,31 @@
+public class Task {
+    private final String task;
+    private boolean done;
+
+    public Task(String task) {
+        this.task = task;
+        this.done = false;
+    }
+
+    public Task(String task, boolean done) {
+        this.task = task;
+        this.done = done;
+    }
+
+    public void markDone() {
+        this.done = true;
+    }
+
+    public void unmarkDone() {
+        this.done = false;
+    }
+
+    @Override
+    public String toString() {
+        if (this.done) {
+            return String.format("[ ] %s", this.task);
+        } else {
+            return String.format("[X] %s", this.task);
+        }
+    }
+}
