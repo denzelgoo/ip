9ff794edb908c87abe2104bbd7bc37a25e61d1db
denzelgoo <denzelgoo05@gmail.com>
2026-08-28 13:44:52 +0800

    Edit code to follow Java coding standard

--- src/main/java/bro/task/Task.java
+++ src/main/java/bro/task/Task.java
@@ -9,11 +9,22 @@ public abstract class Task {
     protected final String task;
     protected boolean isDone;
 
+    /**
+     * Constructs a new Task with the specified description, initially not done.
+     *
+     * @param task The description of the task.
+     */
     public Task(String task) {
         this.task = task;
         this.isDone = false;
     }
 
+    /**
+     * Constructs a new Task with the specified description and completion status.
+     *
+     * @param task   The description of the task.
+     * @param isDone The initial completion status.
+     */
     public Task(String task, boolean isDone) {
         this.task = task;
         this.isDone = isDone;
--- src/test/java/bro/task/TodoTest.java
+++ src/test/java/bro/task/TodoTest.java
@@ -1,12 +1,12 @@
 package bro.task;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit tests for {@link Todo}.
@@ -26,7 +26,7 @@ public class TodoTest {
     public void testMarkDone() {
         Todo todo = new Todo("test todo task");
         todo.markDone();
-        assertTrue(todo.isDone);
+        assertTrue(todo.isDone());
         assertEquals("[T][X] test todo task", todo.toString());
         assertEquals("T | 1 | test todo task", todo.toFileFormat());
     }
@@ -36,13 +36,13 @@ public class TodoTest {
         Todo todo = new Todo("test todo task");
         // mark as done first
         todo.markDone();
-        assertTrue(todo.isDone);
+        assertTrue(todo.isDone());
         assertEquals("[T][X] test todo task", todo.toString());
         assertEquals("T | 1 | test todo task", todo.toFileFormat());
 
         // unmark as done
         todo.unmarkDone();
-        assertFalse(todo.isDone);
+        assertFalse(todo.isDone());
         assertEquals("[T][ ] test todo task", todo.toString());
         assertEquals("T | 0 | test todo task", todo.toFileFormat());
     }
@@ -53,7 +53,7 @@ public class TodoTest {
         // todo is not done, so it should always return true
         LocalDate testDate = LocalDate.of(2026, 8, 28);
         assertTrue(todo.isOnDate(testDate));
-        
+
         // mark as done, then it should return false
         todo.markDone();
         assertFalse(todo.isOnDate(testDate));
