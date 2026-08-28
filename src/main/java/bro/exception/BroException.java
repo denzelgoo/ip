b46eb78b546a17bfef9c61f052486ca843086b0b
denzelgoo <denzelgoo05@gmail.com>
2026-08-28 12:47:13 +0800

    Add header comments to BroException.java

--- src/main/java/bro/exception/BroException.java
+++ src/main/java/bro/exception/BroException.java
@@ -1,6 +1,14 @@
 package bro.exception;
 
+/**
+ * Custom exception thrown for errors specific to the Bro chatbot application.
+ */
 public class BroException extends Exception {
+    /**
+     * Constructs a BroException with the specified error message.
+     * 
+     * @param message The message explaining the cause of the exception.
+     */
     public BroException(String message) {
         super(message);
     }
