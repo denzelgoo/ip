d12da59707b9b6fa3f97674b5da9d5a70721f477
denzelgoo <denzelgoo05@gmail.com>
2026-09-16 19:59:33 +0800

    Align profile pictures to bottom of speech bubble

--- src/main/java/bro/ui/DialogBox.java
+++ src/main/java/bro/ui/DialogBox.java
@@ -78,7 +78,7 @@ public class DialogBox extends HBox {
      * Flips the dialog box such that the ImageView is on the left and text on the right.
      */
     private void flip() {
-        this.setAlignment(Pos.TOP_LEFT);
+        this.setAlignment(Pos.BOTTOM_LEFT);
         ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
         FXCollections.reverse(tmp);
         this.getChildren().setAll(tmp);
